d9365f9555d10324b11bfa00ee4f7f292f2f1955
gus <andres.gonzalezpineda@gmail.com>
2024-12-14 15:46:25 -0600

    cave positioning done

--- src/components/cave.js
+++ src/components/cave.js
@@ -4,10 +4,10 @@ import './cave.css';
 const Cave = (props) => {
     return (    
     <div className='cave-wrapper'>
-        <div className='cave-inside-wrapper'>
-            <img className='cave-inside' src="/cave_imgs/IN.svg" alt="Inside" />
-            <img className='cave-middle' src="/cave_imgs/MID.svg" alt="Middle" />
-        </div>
+        
+        <img className='cave-inside' src="/cave_imgs/IN.svg" alt="Inside" />
+        <img className='cave-middle' src="/cave_imgs/MID.svg" alt="Middle" />
+        
         <div className='cave-outside-wrapper' >
             <img className='cave-outside' src="/cave_imgs/OUT.svg" alt="Outside" />
         </div>        
