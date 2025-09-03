9fb0d3f60017de4c2a9d9ef3754f49dc9fe70318
DiegoDuaS <diego.slowing@gmail.com>
2025-09-02 20:11:14 -0600

    feat: navbar styles and color correction

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './navbar.css';
 
@@ -9,31 +10,36 @@ type NavBarProps = {
 const NavBar: React.FC<NavBarProps> = ({ isLandingPage }) => {
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const handleKeyPress = (e: KeyboardEvent) => {
+            const key = e.key.toLowerCase();
+            if (key === 'b') navigate('/blog');
+            else if (key === 'p') navigate('/podcast');
+            else if (key === 'n') navigate('/aboutus');
+        };
+
+        window.addEventListener('keydown', handleKeyPress);
+
+        // Cleanup para remover el listener cuando el componente se desmonte
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [navigate]);
+
     return (
         <nav className='nav-bar'>
-            
-            <ul>
-                <div>
-                    <img className="header-logo" src="https://raw.githubusercontent.com/Jack-s-Cave/Assets/refs/heads/main/Icons/logo-text.svg" alt="Logo"></img>
-                </div>
-                <li
-                    className='nav-bar-item'
-                    onClick={() => navigate('/blog')}
-                >
-                    [B] Blog
-                </li>
-                <li
-                    className='nav-bar-item'
-                    onClick={() => navigate('/podcast')}
-                >
-                    [P] Podcast
-                </li>
-                <li
-                    className='nav-bar-item'
-                    onClick={() => navigate('/aboutus')}
-                >
-                    [N] Nosotros
-                </li>
+            <div className="nav-left">
+                <img 
+                    className="header-logo" 
+                    src="https://raw.githubusercontent.com/Jack-s-Cave/Assets/refs/heads/main/Icons/logo-text.svg" 
+                    alt="Logo"
+                    onClick={() => navigate('/')}
+                />
+            </div>
+            <ul className="nav-right">
+                <li className='nav-bar-item' onClick={() => navigate('/blog')}>[B] Blog</li>
+                <li className='nav-bar-item' onClick={() => navigate('/podcast')}>[P] Podcast</li>
+                <li className='nav-bar-item' onClick={() => navigate('/aboutus')}>[N] Nosotros</li>
             </ul>
         </nav>
     );
