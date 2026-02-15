de52ab8cc10363d4b7031bf678cf30978123e2ce
nilsmuralles <murallesnils11@gmail.com>
2026-02-15 12:35:21 -0600

    feat(landing): add static blogpost navigation

--- src/components/landingpage/recentCard.tsx
+++ src/components/landingpage/recentCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './recentCard.css';
+import { Link } from 'react-router-dom';
 
 type RecentCardProps = {
   date: string;
@@ -12,24 +13,26 @@ type RecentCardProps = {
 
 const RecentCard: React.FC<RecentCardProps> = ({ date, author, title, imageSrc, tags }) => {
     return (
-        <div className='recent-card'>
+        <Link to={`/blogpost/`}> 
+          <div className='recent-card'>
             <div className='recentC-header'>
-                <p>{date}</p>
-                <p>{author}</p>
+              <p>{date}</p>
+              <p>{author}</p>
             </div>
             <img
-                className='recent-image'
-                src={imageSrc}
-                alt={title}
+              className='recent-image'
+              src={imageSrc}
+              alt={title}
             />
             <h3 className='recentC-title'>{title}</h3>
             <div className='recentC-tags'>
-                {tags.slice(0, 3).map((tag, index) => (
-                    <p key={index}>#{tag}</p>
-                ))}
-                {tags.length > 3 && <p>+{tags.length - 3}</p>}
+              {tags.slice(0, 3).map((tag, index) => (
+                <p key={index}>#{tag}</p>
+              ))}
+              {tags.length > 3 && <p>+{tags.length - 3}</p>}
             </div>
-        </div>
+          </div>
+        </Link>
   );
 };
 
